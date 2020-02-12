eddc17fe51ef6c5b5886568d66ee221e488843b2
Friedger Müffke <mail@friedger.de>
2020-02-12 16:07:27 +0100

    add dna dist

--- ui/src/config.js
+++ ui/src/config.js
@@ -1,7 +1,6 @@
 // console.log(process);
 export const INSTANCE_NAME = 'test-instance';
 export const COURSES_ZOME = 'courses';
-export const ZOME_NAME = 'courses';
 export const MEMBERS_ZOME = 'members';
 export const HOST_URL = `ws://localhost:${process.env.HC_PORT || '8888'}`;
 export const USERNAME = process.env.USERNAME;
